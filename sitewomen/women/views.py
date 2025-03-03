e4bcdab52611177192337008e12a86fcc41610ad
Tapok <y4riv@yandex.ru>
2025-03-03 20:03:35 +0300

    v1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def categories(request):
+    return HttpResponse("<h1>gg wp</h1>")
