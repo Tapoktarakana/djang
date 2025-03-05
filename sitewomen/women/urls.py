13766db66ded5a74f49a3e785259709ff36741dd
Tapok <y4riv@yandex.ru>
2025-03-05 14:16:07 +0300

    v1

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, register_converter
 
 from . import views
 
+from . import converters
+register_converter(converters.FourDigitYearConverter,'year4')
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cats/', views.categories, name='categories'),
+    path('cats/<int:cat_id>/', views.categories, name='categories'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='categories'),
+    path('archive/<year4:year>/', views.archive),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Страница приложения women.")
 
-def categories(request):
-    return HttpResponse("<h1>gg wp</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat_id}</p></>")
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{cat_slug}</p></>')
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{year}</p></>')
