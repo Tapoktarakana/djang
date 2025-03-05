ac67aa0787ffbf5b2818fbfeee7c79a15a949d10
Tapok <y4riv@yandex.ru>
2025-03-05 17:31:39 +0300

    v1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def index(request):
     return HttpResponse("Страница приложения women.")
 
+
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat_id}</p></>")
+
 
 def categories_by_slug(request, cat_slug):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>{cat_slug}</p></>')
 
+
 def archive(request, year):
+    if year > 2023:
+        url_redirect = reverse('cats', args=('music', ))
+        return redirect(url_redirect)
+
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>{year}</p></>')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound(f'<h1>Не найдено</h1>')
