244e6768454d53ae990b125cf9e9f137b0d0897b
Tapok <y4riv@yandex.ru>
2025-03-07 14:03:29 +0300

    v1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,12 +2,27 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 def index(request):
-    return render(request, 'women/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html',{'title':'О сайте'})
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat_id}</p></>")
