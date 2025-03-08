ea20fe5b41430669239ac86d2548c03aa2575bb8
Tapok <y4riv@yandex.ru>
2025-03-08 13:45:32 +0300

    v1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,7 +8,7 @@ menu = ["О сайте", "Добавить статью", "Обратная св
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+    {'id': 5, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
 def index(request):
@@ -22,21 +22,11 @@ def index(request):
 def about(request):
     return render(request, 'women/about.html',{'title':'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat_id}</p></>")
-
-
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{cat_slug}</p></>')
-
-
-def archive(request, year):
-    if year > 2023:
-        url_redirect = reverse('cats', args=('music', ))
-        return redirect(url_redirect)
-
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{year}</p></>')
 
 
 def page_not_found(request, exception):
     return HttpResponseNotFound(f'<h1>Не найдено</h1>')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
