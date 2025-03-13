8844da75a3c36742c3f82306a50d6aede8ab686c
Tapok <y4riv@yandex.ru>
2025-03-13 21:24:07 +0300

    v1

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,7 +4,7 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 import women
-from women.models import Women, Category
+from women.models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -21,6 +21,7 @@ def index(request):
         'title': 'Главная страница',
         'menu': menu,
         'posts': posts,
+        'cat_selected': 0,
     }
 
     return render(request, 'women/index.html', context=data)
@@ -70,3 +71,17 @@ def show_category(request, cat_slug):
     }
 
     return render(request, 'women/index.html', context=data)
+
+
+def show_tag_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+    data = {
+        'title': f'Тег: {tag.tag}',
+        'menu': menu,
+        'posts': posts,
+        'cat_selected': None,
+    }
+
+    return render(request, 'women/index.html', context=data)
+
